1d1ccc54010ecef9d9096f6afa2c3109e8fd97e9
Stephen <hellostephenwm@gmail.com>
2024-05-15 18:52:33 -0400

    fixed notes filtering in delete method

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.get("/api/notes/:id", (request, response) => {
 
 app.delete("/api/notes/:id", (request, response) => {
 	const id = Number(request.params.id);
-	notes.filter((note) => note.id !== id);
+	notes = notes.filter((note) => note.id !== id);
 
 	response.status(204).end();
 });
