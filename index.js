bad367276d6ec7e090b2ab51806445f5dfe244b5
Stephen <hellostephenwm@gmail.com>
2024-05-15 15:50:08 -0400

    Added method to allow creating new notes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const PORT = 3001;
 
 let notes = [
 	{
@@ -18,6 +19,8 @@ let notes = [
 		important: true,
 	},
 ];
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
 	response.send("<h1>Hello World!</h1>");
@@ -40,14 +43,37 @@ app.get("/api/notes/:id", (request, response) => {
 	}
 });
 
+const generateId = () => {
+	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+	return maxId + 1;
+};
+
+app.post("/api/notes/", (request, response) => {
+	const body = request.body;
+
+	if (!body.content || body.content === "") {
+		return response.status(400).json({
+			error: "missing content",
+		});
+	}
+
+	const note = {
+		content: body.content,
+		important: Boolean(body.important) || false,
+		id: generateId(),
+	};
+
+	notes = notes.concat(note);
+
+	response.json(note);
+});
+
 app.delete("/api/notes/:id", (request, response) => {
 	const id = Number(request.params.id);
 	notes.filter((note) => note.id !== id);
 
 	response.status(204).end();
 });
-
-const PORT = 3001;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
