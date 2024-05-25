36ea55ab2a32216324026daf5c90d0c6994bfc10
Stephen <hellostephenwm@gmail.com>
2024-05-25 18:41:22 -0400

    modified delete method and added put method to change note important

--- index.js
+++ index.js
@@ -62,11 +62,27 @@ app.post("/api/notes/", (request, response) => {
 	});
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-	const id = Number(request.params.id);
-	notes.filter((note) => note.id !== id);
+app.delete("/api/notes/:id", (request, response, next) => {
+	Note.findByIdAndDelete(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
+});
 
-	response.status(204).end();
+app.put("/api/notes/:id", (request, response, next) => {
+	const body = request.body;
+
+	const note = {
+		content: body.content,
+		important: body.important,
+	};
+
+	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+		.then((updatedNote) => {
+			response.json(updatedNote);
+		})
+		.catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response, next) => {
