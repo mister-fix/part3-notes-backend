d27a14f8d20d15ab9442160d18a11dc4ac2aea2d
Stephen <hellostephenwm@gmail.com>
2024-05-26 16:56:48 -0400

    updated post method and error handler for note validation

--- index.js
+++ index.js
@@ -43,23 +43,20 @@ app.get("/api/notes/:id", (request, response, next) => {
 		.catch((error) => next(error));
 });
 
-app.post("/api/notes/", (request, response) => {
+app.post("/api/notes/", (request, response, next) => {
 	const body = request.body;
-
-	if (body.content === undefined) {
-		return response.status(400).json({
-			error: "missing content",
-		});
-	}
 
 	const note = new Note({
 		content: body.content,
 		important: body.important || false,
 	});
 
-	note.save().then((savedNote) => {
-		response.json(savedNote);
-	});
+	note
+		.save()
+		.then((savedNote) => {
+			response.json(savedNote);
+		})
+		.catch((error) => next(error));
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
@@ -99,6 +96,8 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === "CastError") {
 		return response.status(400).send({ error: "malformatted id" });
+	} else if (error.name === "ValidationError") {
+		return response.status(400).json({ error: error.message });
 	}
 
 	next(error);
