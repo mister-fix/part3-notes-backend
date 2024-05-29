b784cc9571da51f86c4579f36fb338e9a9205851
Stephen <hellostephenwm@gmail.com>
2024-05-29 15:24:53 -0400

    removed error and endpoint middleware to own module

--- app.js
+++ app.js
@@ -1,48 +1,33 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
-const logger = require('./utils/logger')
-const notesRouter = require('./controllers/notes')
-
-const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:', request.path)
-  logger.info('Body:', request.body)
-  logger.info('---')
-  next()
-}
-
 const cors = require('cors')
+const mongoose = require('mongoose')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 app.use(cors())
-
 app.use(express.static('dist'))
 app.use(express.json())
-app.use(requestLogger)
+app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 
-const unknownEndpoint = (request, response, next) => {
-  response.status(404).send({
-    error: 'unknown endpoint',
-  })
-}
-
-// handler of requests with unkown endpoint
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-//handler of requests with result to errors
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
