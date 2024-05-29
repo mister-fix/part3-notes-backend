7723c657bc8ca13bb7e670d45deb95004238363a
Stephen <hellostephenwm@gmail.com>
2024-05-28 22:04:55 -0400

    moved mongo connection and schema definition to own models file

--- models/note.js
+++ models/note.js
@@ -1,35 +1,35 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+const logger = require('../utils/logger')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to database...");
+logger.info('connecting to database...')
 
 mongoose
-	.connect(url)
-	.then((result) => {
-		console.log("connected to MongoDB");
-	})
-	.catch((error) => {
-		console.log("error connecting to MongoDB", error.message);
-	});
+  .connect(config.MONGODB_URI)
+  .then((result) => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
 
 const noteSchema = new mongoose.Schema({
-	content: {
-		type: String,
-		minLength: 5,
-		required: true,
-	},
-	important: Boolean,
-});
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  important: Boolean,
+})
 
-noteSchema.set("toJSON", {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
-});
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
