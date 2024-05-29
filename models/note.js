e2a5bcbacbb16f0be8324f88fda7037cebcb8500
Stephen <hellostephenwm@gmail.com>
2024-05-29 15:25:04 -0400

    removed mongoose connection code

--- models/note.js
+++ models/note.js
@@ -1,19 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-const logger = require('../utils/logger')
-
-mongoose.set('strictQuery', false)
-
-logger.info('connecting to database...')
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then((result) => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
