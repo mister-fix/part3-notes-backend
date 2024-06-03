9167e163609bc0e0a4cee0dbc6f9b3caf6370c00
Stephen <hellostephenwm@gmail.com>
2024-06-03 19:13:16 -0400

    modified tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,4 +1,17 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -16,7 +29,7 @@ test('notes are returned as json', async () => {
 test('there are two notes', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 // test('the first note is about HTTP methods', async () => {
@@ -31,6 +44,14 @@ test('the first note is about HTTP methods', async () => {
 
   const contents = response.body.map((e) => e.content)
   assert(contents.includes('HTML is easy'))
+})
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
 })
 
 after(async () => {
