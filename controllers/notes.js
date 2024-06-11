6f31a2e0d6aebee87a3c7cdfe21cbb5f2c971432
Stephen <hellostephenwm@gmail.com>
2024-06-10 20:50:37 -0400

    updated note post method to use jwt token

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
@@ -16,22 +17,34 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
     important: body.important === undefined ? false : body.important,
-    user: user.id,
+    user: user._id,
   })
 
   const savedNote = await note.save()
   user.notes = user.notes.concat(savedNote._id)
   await user.save()
 
-  response.status(201).json(savedNote)
+  response.json(savedNote)
 })
 
 notesRouter.delete('/:id', async (request, response) => {
