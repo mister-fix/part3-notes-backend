e50592065f63720164619341900e40d3071473a1
Stephen <hellostephenwm@gmail.com>
2024-07-22 15:20:10 -0400

    test endpoint controller

---
+++ controllers/testing.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const Note = require('../models/note')
+const User = require('../models/user')
+
+router.post('/reset', async (request, response) => {
+  await Note.deleteMany({})
+  await User.deleteMany({})
+
+  response.status(20).end()
+})
+
+module.exports = router
